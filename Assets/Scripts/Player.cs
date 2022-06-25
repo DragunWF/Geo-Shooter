967f0bcfd5875900a544b131744114e4a92c2ac9
dragunwf <plarisanmarc@gmail.com>
2022-06-26 01:08:18 +0800

    Implemented basic controls and rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const float speed = 255.5f;
+    private Vector2 rawInput;
+
+    private Rigidbody2D rigidBody;
+
+    private void Awake()
     {
-        
+        rigidBody = FindObjectOfType<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+    }
+
+    private void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+    }
+
+    private void Move()
+    {
+        transform.Rotate(0, 0, rawInput.x * speed * Time.deltaTime);
     }
 }
