413e5f6ebd89161859d841a03569347f9ea7c794
dragunwf <plarisanmarc@gmail.com>
2022-06-26 19:58:11 +0800

    Implemented basic bullet functionality

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private ParticleSystem hitEffect;
+
+    private void Awake()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.tag == "Enemy")
+            Destroy(gameObject);
     }
 }
