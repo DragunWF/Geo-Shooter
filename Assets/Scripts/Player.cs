5da94af5feefb0bb1bd249c886630bc7595fdc4e
dragunwf <plarisanmarc@gmail.com>
2022-06-26 18:40:07 +0800

    Implemented player shooting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,10 +8,16 @@ public class Player : MonoBehaviour
     private Rigidbody2D rigidBody;
     private Vector2 mousePos;
 
+    private Transform firePoint;
+    private GameObject bulletPrefab;
+    private float bulletForce = 22.5f;
+
     private void Awake()
     {
         rigidBody = FindObjectOfType<Rigidbody2D>();
         mainCamera = FindObjectOfType<Camera>();
+        bulletPrefab = Resources.Load("Prefabs/Bullet") as GameObject;
+        firePoint = GameObject.Find("FirePoint 1").transform;
     }
 
     private void Update()
@@ -20,19 +26,27 @@ public class Player : MonoBehaviour
         Shooting();
     }
 
-    private void Shooting()
-    {
-
-    }
-
     private void AimTowardsMouse()
     {
         mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         Vector2 lookDirection = mousePos - rigidBody.position;
 
-        float rotationOffset = 90;
+        const float rotationOffset = 90;
         float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - rotationOffset;
 
         rigidBody.rotation = angle;
+    }
+
+    private void Shooting()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D bulletRigidBody = bullet.GetComponent<Rigidbody2D>();
+            bulletRigidBody.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+
+            const float bulletDespawnTime = 6.5f;
+            Destroy(bullet, bulletDespawnTime);
+        }
     }
 }
