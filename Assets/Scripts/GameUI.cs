9486cd9b9166a0a15fc85cd9c511e476e21657c4
dragunwf <plarisanmarc@gmail.com>
2022-06-27 00:24:51 +0800

    Reordered methods and optimized code performance

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -11,9 +11,31 @@ public class GameUI : MonoBehaviour
     private EnemySpawner enemySpawner;
     private AudioPlayer audioPlayer;
 
+    public void UpdateScoreText(int scoreAmount)
+    {
+        if (scoreAmount >= 1000)
+        {
+            char[] scoreTextArray = scoreAmount.ToString().ToCharArray();
+            List<char> formattedScore = new List<char>();
+            Array.Reverse(scoreTextArray);
+
+            for (int i = 0; i < scoreTextArray.Length; i++)
+            {
+                formattedScore.Add(scoreTextArray[i]);
+                if ((i + 1) % 3 == 0 && i + 1 != scoreTextArray.Length)
+                    formattedScore.Add(',');
+            }
+
+            formattedScore.Reverse();
+            scoreText.text = string.Join("", formattedScore);
+        }
+        else
+            scoreText.text = scoreAmount.ToString();
+    }
+
     public void UpdateDifficultyText(int difficultyLevel)
     {
-        if (difficultyLevel >= enemySpawner.GetMaxDifficultyLevel())
+        if (difficultyLevel == enemySpawner.GetMaxDifficultyLevel())
         {
             difficultyText.text = "Max";
             // Add sound effect
@@ -23,23 +45,6 @@ public class GameUI : MonoBehaviour
             difficultyText.text = difficultyLevel.ToString();
             // Add sound effect
         }
-    }
-
-    public void UpdateScoreText(int scoreAmount)
-    {
-        char[] scoreTextArray = scoreAmount.ToString().ToCharArray();
-        List<char> formattedScore = new List<char>();
-        Array.Reverse(scoreTextArray);
-
-        for (int i = 0; i < scoreTextArray.Length; i++)
-        {
-            formattedScore.Add(scoreTextArray[i]);
-            if ((i + 1) % 3 == 0 && i + 1 != scoreTextArray.Length)
-                formattedScore.Add(',');
-        }
-
-        formattedScore.Reverse();
-        scoreText.text = string.Join("", formattedScore);
     }
 
     private void Awake()
