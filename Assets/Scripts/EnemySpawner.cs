6afc8a98f36f26ad49acec8e438e140e0ee57230
dragunwf <plarisanmarc@gmail.com>
2022-06-26 19:00:22 +0800

    Implemented basic setup for functionality for EnemySpawner.cs

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject enemyPrefab;
+    private List<Transform> spawnPoints;
+
+    private float maxSpawnInterval = 12.5f;
+    private float minSpawnInterval = 6.25f;
+    private float spawnInterval;
+
+    private void Awake()
     {
-        
+        enemyPrefab = Resources.Load("Prefabs/Enemy") as GameObject;
+
+        const int pointIndexLimit = 8;
+        for (int i = 0; i < pointIndexLimit; i++)
+        {
+            GameObject point = GameObject.Find(string.Format("Point {0}", i));
+            spawnPoints.Add(point.transform);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        yield return new WaitForSeconds(spawnInterval);
     }
 }
