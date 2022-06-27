c7db0f26640f74496840d879562ffbbc098c5146
dragunwf <plarisanmarc@gmail.com>
2022-06-27 15:37:03 +0800

    Implemented score saving system and high score mechanic

--- Assets/Scripts/GameInfo.cs
+++ Assets/Scripts/GameInfo.cs
@@ -7,8 +7,10 @@ public class GameInfo : MonoBehaviour
     public int Score { get; private set; }
     public int HighScore { get; private set; }
     public string FactionChosen { get; private set; }
+    public bool ReachedNewHighScore { get; private set; }
 
     private GameUI gameUI;
+    private RetryMenuUI retryMenuUI;
 
     public void IncreaseScore(int gainAmount)
     {
@@ -16,12 +18,29 @@ public class GameInfo : MonoBehaviour
         gameUI.UpdateScoreText(Score);
     }
 
+    public void SaveScore()
+    {
+        if (Score > HighScore)
+        {
+            ReachedNewHighScore = true;
+            HighScore = Score;
+        }
+    }
+
+    public void OnGameRestart()
+    {
+        Score = 0;
+        ReachedNewHighScore = false;
+    }
+
     private void Awake()
     {
         Score = 0;
         HighScore = 0;
         FactionChosen = "";
+        ReachedNewHighScore = false;
 
         gameUI = FindObjectOfType<GameUI>();
+        retryMenuUI = FindObjectOfType<RetryMenuUI>();
     }
 }
